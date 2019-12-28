37e625f68651924a6782401d317269940d78d889
Brian Cohan <briancohan@gmail.com>
2019-12-28 12:38:14 -0500

    organized config files

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,25 +2,25 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Brian Cohan'
+# Site Info
+AUTHOR = 'SFPECVA'
 SITENAME = 'SFPE Central VA'
 SITEURL = ''
-
-PATH = 'content'
-THEME = 'themes/sfpecva'
-DEFAULT_PAGINATION = 10
-DELETE_OUTPUT_DIRECTORY = True
-
 TIMEZONE = 'America/New_York'
-
 DEFAULT_LANG = 'en'
 
-# Feed generation is usually not desired when developing
+# Disable Feeds
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+
+# Rendering Info
+PATH = 'content'
+THEME = 'themes/sfpecva'
+DEFAULT_PAGINATION = 10
+DELETE_OUTPUT_DIRECTORY = True
 
 STATIC_PATHS = [
     'images',
@@ -31,16 +31,3 @@ EXTRA_PATH_METADATA = {
     'extra/favicon.ico': {'path': 'favicon.ico'},
     'extra/CNAME': {'path': 'CNAME'},
 }
-
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
